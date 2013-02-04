6b9d9d13db306012732af52f1cc8e12219632cf8
Krum Bakalsky <krum.bakalsky@sap.com>
2013-02-04 13:00:53 +0200

    Comment out a method which is no longer present from AR-JDBC Adapter 1.2.6
    Signed-off-by: Krum Bakalsky <krum.bakalsky@sap.com>

--- activerecord-maxdb-adapter/lib/arjdbc/maxdb.rb
+++ activerecord-maxdb-adapter/lib/arjdbc/maxdb.rb
@@ -1,4 +1,8 @@
 require 'arjdbc/jdbc'
-jdbc_require_driver 'jdbc/maxdb'
+
+# This is not needed since AR-JDBC Adapter version 1.2.6, and moreover causes an error.
+# Please uncomment it for versions prior to 1.2.6
+# jdbc_require_driver 'jdbc/maxdb'
+
 require 'arjdbc/maxdb/connection_methods'
 require 'arjdbc/maxdb/adapter'
