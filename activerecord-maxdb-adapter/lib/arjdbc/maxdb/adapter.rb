5267a184bbfcc33d7cdbc48ba48b13b642fc5191
Krum Bakalsky <krum.bakalsky@sap.com>
2013-02-14 16:29:33 +0200

    Improvements.
    Signed-off-by: Krum Bakalsky <krum.bakalsky@sap.com>

--- activerecord-maxdb-adapter/lib/arjdbc/maxdb/adapter.rb
+++ activerecord-maxdb-adapter/lib/arjdbc/maxdb/adapter.rb
@@ -3,7 +3,8 @@ require 'arel/visitors/maxdb'
 module ::ArJdbc
 
   module MaxDB
-    # This defines an Arel visitor for our adapter. See https://github.com/jruby/activerecord-jdbc-adapter/issues/137
+  
+    # This defines an Arel visitor for the adapter. See https://github.com/jruby/activerecord-jdbc-adapter/issues/137
 	def self.arel2_visitors(config)
 	  { 'jdbc' => ::Arel::Visitors::MaxDB }
 	end
@@ -12,7 +13,7 @@ module ::ArJdbc
 	def create_table(name, options = {})
 	  super(name, options)
 	  execute("CREATE SEQUENCE #{default_sequence_name(name)} START WITH 1 INCREMENT BY 1")
-    end
+	end
 	
 	# ... and we need to take care for the clean up.
 	def drop_table(name)
@@ -20,9 +21,10 @@ module ::ArJdbc
 	  execute("DROP SEQUENCE #{default_sequence_name(name)}")
 	end
 	
-	def default_sequence_name(table_name, primary_key = nil) # :nodoc:
+	# This gives the names we assign to sequences.
+	def default_sequence_name(table_name, primary_key = nil)
 	  "#{table_name}_seq"
-    end
+	end
 	
 	# Get the next value from the sequence. See http://maxdb.sap.com/doc/7_8/44/e15c6499fc03fde10000000a1553f6/content.htm
 	def next_sequence_value(sequence_name)
@@ -42,8 +44,8 @@ module ::ArJdbc
 	  sql << " DEFAULT #{quote(options.delete(:default))}" if options.has_key?(:default)
 	  super
 	end
-	
-	def change_column_default(table_name, column_name, default) #:nodoc:
+
+	def change_column_default(table_name, column_name, default)
 	  column = column_for(table_name, column_name)
 	  change_column table_name, column_name, column.sql_type, :default => default
 	end
@@ -51,11 +53,9 @@ module ::ArJdbc
 	# Handle correctly some boolean literals.
 	def quote(value, column = nil)
 	  return value.quoted_id if value.respond_to?(:quoted_id)
-	  
 	  if column && column.respond_to?(:primary) && column.primary && column.klass != String
 	    return value.to_i.to_s
 	  end
-	  
 	  case value
 	    when TrueClass then 'true'
 		when FalseClass then 'false'
@@ -64,78 +64,73 @@ module ::ArJdbc
 	end
 	
 	# The default generated index names can be too long for MaxDB, so we override the add_index method
-	# and use the idx_tableName_columnName pattern for naming the indexes. This worked for our use case.
+	# and use the idx_tableName_columnName pattern for naming the indexes.
 	
 	def add_index(table_name, column_name, options = {})
-
-      # In case the index is a composite one, the column_name argument is an
-      # array of all the column names that constitute the index
-      if column_name.is_a? (Array)
-        suffix_name = column_name.join('_')
-        column_list = column_name.join(", ")
-      else
-        suffix_name = column_name
-        column_list = column_name
-      end
-
+	
+	  # In case the index is a composite one, the column_name argument is an
+	  # array of all the column names that constitute the index
+	  if column_name.is_a? (Array)
+	    suffix_name = column_name.join('_')
+		column_list = column_name.join(", ")
+	  else
+	    suffix_name = column_name
+		column_list = column_name
+	  end
+	  
+	  index_name = "idx_" + "#{table_name}" + "_" + "#{suffix_name}"
+	  
 	  statement = "CREATE"
 	  statement << " UNIQUE " if options[:unique]
-	  statement << " INDEX idx_" + "#{table_name}" + "_" + "#{suffix_name}"
+	  statement << " INDEX " + index_name[0..30] 	# cut out the index name in case it becomes too long
 	  statement << " ON #{table_name}(#{column_list})"
 	  execute statement
     end
 	
-	# We got a 'non existing method' error, so we had to implement this method as well. Basically, it introduces a NULL
-    # or a NOT NULL constraint on a given table and column, depending on the value of the boolean 'null' parameter
-    
-    def change_column_null(table_name, column_name, null)
-      if null
-        execute "ALTER TABLE #{table_name} MODIFY (#{column_name} NULL)"
-      else
-        execute "ALTER TABLE #{table_name} MODIFY (#{column_name} NOT NULL)"
-      end
-    end
+	def change_column_null(table_name, column_name, null)
+	  if null
+	    execute "ALTER TABLE #{table_name} MODIFY (#{column_name} NULL)"
+	  else
+	    execute "ALTER TABLE #{table_name} MODIFY (#{column_name} NOT NULL)"
+	  end
+	end
 	
 	# We use eagerly prefetching of primary keys in order to get ActiveRecord synced with the auto generated primary key
 	# in the database.
 	def prefetch_primary_key?(table_name = nil)
 	  true
 	end
-
-    def adapter_name #:nodoc:
-      'maxdb'
-    end
-
+	
+	def adapter_name
+	  'maxdb'
+	end
   end
-  
 end
 
 module ActiveRecord
   module ConnectionAdapters
   
     # Metadata model for a MaxDB column.
-    class MaxDBColumn < JdbcColumn
-
+	class MaxDBColumn < JdbcColumn
+	
 	  # The default value of a primary key column is a very strange string, which eventually becomes a zero - 0 -
 	  # for ActiveRecord. But we have to use 'nil' so that ActiveRecord fetches the newly generated pk value when
-      # an INSERT statement has been executed.
-      def default_value(val)
-        if String === val && val.include?("DEFAULT SERIAL")
-          nil
-        else
-          val
-        end
-      end
-    end
-
-    class MaxDBSQLAdapter < JdbcAdapter
+	  # an INSERT statement has been executed.
+	  def default_value(val)
+	    if String === val && val.include?("DEFAULT SERIAL")
+		  nil
+		else
+		  val
+		end
+	  end
+	end
+	
+	class MaxDBSQLAdapter < JdbcAdapter
       include ArJdbc::MaxDB
 	  
-      def jdbc_column_class
-        ActiveRecord::ConnectionAdapters::MaxDBColumn
-      end
-	  
-    end
-
+	  def jdbc_column_class
+	    ActiveRecord::ConnectionAdapters::MaxDBColumn
+	  end
+	end
   end
 end
